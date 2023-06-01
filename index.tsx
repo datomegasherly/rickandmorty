7eeedef25b58e53dcd015e06c0bb2f8ee0311d6e
datomegasherly <dat.omega.sherly@gmail.com>
2023-06-02 01:30:22 +0300

    Add graphql-request to call graphql api

--- index.tsx
+++ index.tsx
@@ -3,14 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./src/App";
 import { Provider } from "react-redux";
 import store from "./src/redux/store";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLInputElement
 );
+
+const client = new QueryClient();
+
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
-    <App />
+    <QueryClientProvider client={client}>
+      <App />
+    </QueryClientProvider>
   </Provider>
   // </React.StrictMode>
 );
